5edf8cd9882a65b326b28c192979576d0acf94bf
Twti01 <davidlube4@gmail.com>
2024-08-17 14:36:29 +0200

    new_chang

--- multi_edit.py
+++ multi_edit.py
@@ -304,4 +304,4 @@ def update_medal_bar_figure(sport, game, sex, season, country):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
